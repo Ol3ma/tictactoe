ca9b64f82614b6fa105cb1d32454775b3461dfc4
Schnuppertag <mahlstedt.ole@outlook.com>
2023-10-16 16:32:45 +0200

    Spielfeld fertig nicht getestet

--- src/main/java/ole/praktikum/Spielfeld.java
+++ src/main/java/ole/praktikum/Spielfeld.java
@@ -1,5 +1,22 @@
 package ole.praktikum;
 
+
 public class Spielfeld {
 
+    private int[][] feld_arr = new int[3][3]; // 0 gleich leer, 1 gleich kreuz, 2 gleich kreis
+
+    /**
+     * 0 = leeres Feld
+     * 1 = kreuz
+     * 2 = kreis
+     * @param x
+     * @param y
+     * @return
+     */
+    public int abfragespielstein(int x,int y){
+        return feld_arr[x][y];
+    }
+    public void setztenspielstein(int x,int y,int wert){
+        feld_arr[x][y] = wert;
+    }
 }//enthält koordinaten 3x3 mit dem Zustand des Feldes
