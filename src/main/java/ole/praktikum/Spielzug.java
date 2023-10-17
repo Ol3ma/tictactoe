c836559b6d0f045dc8692456bfd5641b5aa0de4a
Schnuppertag <mahlstedt.ole@outlook.com>
2023-10-17 09:12:50 +0200

    Spielzug weiter Optimirt

--- src/main/java/ole/praktikum/Spielzug.java
+++ src/main/java/ole/praktikum/Spielzug.java
@@ -4,18 +4,17 @@ import java.util.Scanner;
 
 public class Spielzug {
     public int[] zug(boolean spieler) {
-        boolean[] fertig = {false, false};
         int[] koordinaten = new int[2];
 
 
         Scanner myObj = new Scanner(System.in);
         String position = myObj.nextLine();
-        char[] char_position = position.toCharArray();
         koordinaten[0] = position.charAt(0);
         koordinaten[1] = position.charAt(1);
         return koordinaten;
 
     /*
+    boolean[] fertig = {false, false};
     while (fertig[0] && fertig[1]) {
         if (spieler = true) {
             System.out.println("Auf welches Feld willst du ein Kreuz setzten?");
@@ -25,7 +24,6 @@ public class Spielzug {
 
         Scanner myObj = new Scanner(System.in);
         String position = myObj.nextLine();
-        char[] char_position = position.toCharArray();
         if (position.charAt(0) == 'a') {
             koordinaten[0] = 0;
             fertig[0]=true;
@@ -37,13 +35,13 @@ public class Spielzug {
             fertig[0]=true;
         }
 
-        if (char_position.charAt(1) == "1") {
+        if (position.charAt(1) == "1") {
             koordinaten[1] = 0;
             fertig[1]=true;
-        } else if (char_position.charAt(1) == "2") {
+        } else if (position.charAt(1) == "2") {
             koordinaten[1] = 1;
             fertig[1]=true;
-        } else if (char_position.charAt(1) == "3") {
+        } else if (position.charAt(1) == "3") {
             koordinaten[1] = 2;
             fertig[1]=true;
         }
