1339bf68989e056f5bd2bec570ff3cdeb36e222b
Schnuppertag <mahlstedt.ole@outlook.com>
2023-10-17 13:16:01 +0200

    Unnötige Kommentare entfernt

--- src/main/java/ole/praktikum/Spielzug.java
+++ src/main/java/ole/praktikum/Spielzug.java
@@ -55,43 +55,5 @@ public class Spielzug {
         else {
             spielfeld.setztenspielstein(koordinaten[0],koordinaten[1],2 );
         }
-
-    /*
-    boolean[] fertig = {false, false};
-    while (fertig[0] && fertig[1]) {
-        if (spieler = true) {
-            System.out.println("Auf welches Feld willst du ein Kreuz setzten?");
-        } else {
-            System.out.println("Auf welches Feld willst du ein Kreis setzten?");
-        }
-
-        Scanner myObj = new Scanner(System.in);
-        String position = myObj.nextLine();
-        if (position.charAt(0) == 'a') {
-            koordinaten[0] = 0;
-            fertig[0]=true;
-        } else if (position.charAt(0) == 'b') {
-            koordinaten[0] = 1;
-            fertig[0]=true;
-        } else if (position.charAt(0) == 'c') {
-            koordinaten[0] = 2;
-            fertig[0]=true;
-        }
-
-        if (position.charAt(1) == "1") {
-            koordinaten[1] = 0;
-            fertig[1]=true;
-        } else if (position.charAt(1) == "2") {
-            koordinaten[1] = 1;
-            fertig[1]=true;
-        } else if (position.charAt(1) == "3") {
-            koordinaten[1] = 2;
-            fertig[1]=true;
-        }
-
-    }
-
-}
-*/
     }
 }//Prüft ob das Feld noch frei ist. Informationen vom Spielfeld
