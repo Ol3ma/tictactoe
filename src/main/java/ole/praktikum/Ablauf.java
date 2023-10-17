0c6ae44102c90e3a7013f99618dabab2f1af7929
Schnuppertag <mahlstedt.ole@outlook.com>
2023-10-17 11:20:53 +0200

    Ablauf funktioniert muss noch verschönert werden

--- src/main/java/ole/praktikum/Ablauf.java
+++ src/main/java/ole/praktikum/Ablauf.java
@@ -2,4 +2,21 @@ package ole.praktikum;
 
 public class Ablauf {
 
+    public static void main(String[] args) {
+        boolean spielereinsdran = true;
+        Spielfeld spielfeld = new Spielfeld();
+        Pruefenobgewonnen pruefenobgewonnen = new Pruefenobgewonnen();
+        Spielzug spielzug = new Spielzug();
+        while (pruefenobgewonnen.prüfen(spielfeld) == 0) {
+            spielzug.zug(spielfeld, spielereinsdran);
+
+            spielereinsdran = !spielereinsdran;
+
+            System.out.println(spielfeld.abfragespielstein(0,0)+" | "+spielfeld.abfragespielstein(1,0)+" | "+spielfeld.abfragespielstein(2,0));
+            System.out.println("-----------");
+            System.out.println(spielfeld.abfragespielstein(0,1)+" | "+spielfeld.abfragespielstein(1,1)+" | "+spielfeld.abfragespielstein(2,1));
+            System.out.println("-----------");
+            System.out.println(spielfeld.abfragespielstein(0,2)+" | "+spielfeld.abfragespielstein(1,2)+" | "+spielfeld.abfragespielstein(2,2));
+        }
+    }
 }//enthält welcher Spieler am zug ist
